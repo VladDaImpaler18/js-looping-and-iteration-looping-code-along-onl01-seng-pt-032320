52341f9ccc096b4aabc0b47e192e809de557ee24
Vlad <VladDaImpaler@gmail.com>
2020-09-27 15:37:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+// writeCards(["Ada", "Brendan", "Ali"], "birthday");
+function writeCards(names, shindig){
+    let output = [];
+    for(let i = 0; i < names.length; i++)
+    {
+        output.push(`Thank you, ${names[i]}, for the wonderful ${shindig} gift!`);
+    }
+    return output;
+}
+
+function countDown(number){
+    while (number >= 0)
+    {
+        console.log(number);
+        --number;
+    }
+
+}
